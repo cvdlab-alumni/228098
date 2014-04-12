8e7a894518b168ffab84ad878dbb136b1984680a
j-hard <and.gia.83@gmail.com>
2014-04-12 20:47:16 +0200

    terzo commit

--- 2014-03-21/python/esercizio4.py
+++ 2014-03-21/python/esercizio4.py
@@ -72,7 +72,7 @@ west=ovest
 
 prospects=STRUCT([COLOR(BLACK)(north),COLOR(GREEN)(south),COLOR(YELLOW)(east),west])
 #prospects=STRUCT([north,south,east,west])
-VIEW(prospects)
+#VIEW(prospects)
 
 pts0=[[-20,-20],[70,-20],[70,70],[-20,70]]
 pts1=[[0,0],[53,0],[53,53],[0,53]]
@@ -84,13 +84,10 @@ pols=None
 
 ptsscala=[[0,0],[0.2,0],[0.2, -0.2],[0,-0.2]]
 ptscorrimano=[[0,1],[0.2,1],[20, -19],[20,-20],[0,-20]]
-#gradino=PROD([MKPOL([ptsscala,cells,pols]),Q(10)])
-#gradino2D=MKPOL([ptsscala,cells,pols])
-#scala2D=STRUCT([T([1,2,3])([0.2*(x-1),-0.2*(x-1),0])(gradino2D) for x in range(20)])
-#scala3D=PROD([scala2D, Q(70)])
+
 gradino3D=PROD([MKPOL([ptsscala,cells,pols]), Q(93)])
 corrimano3D=PROD([MKPOL([ptscorrimano,cells2,pols]), Q(0.2)])
-VIEW(STRUCT([corrimano3D]))
+#VIEW(STRUCT([corrimano3D]))
 scala3D0=STRUCT([T([1,2,3])([0.2*(x-1),-0.2*(x-1),0])(gradino3D) for x in range(101)])
 scala3D=STRUCT([scala3D0,corrimano3D,T([1,2,3])([0,0,92.8])(corrimano3D)])
 scalaEst=T([1,2,3])([-20,73,-0.2])(MAP([S3,S1,S2])(scala3D))
@@ -98,11 +95,6 @@ scalaEst=T([1,2,3])([-20,73,-0.2])(MAP([S3,S1,S2])(scala3D))
 scalaNord=T([1,2,3])([0,53,0])(R([1,2])(-PI/2)(scalaEst))
 scalaSud=T([1,2,3])([53,0,0])(R([1,2])(PI/2)(scalaEst))
 scalaOvest=T([1,2,3])([53,53,0])(R([1,2])(PI)(scalaEst))
-
-#scalaNord=T([1,2,3])([(70+20-0.2),-20,(0.2-20)])(MAP([S2,S3,S1])(scala3D))
-#scalaSud=T([1,2,3])([50,50,0])(R([1,2])(PI)(scalaNord))
-#scalaOvest=T([1,2,3])([50,(70-20),0])(R([1,2])(PI)(scalaEst))
-#scale=JOIN([COLOR(BLACK)(scalaNord),COLOR(GREEN)(scalaSud)])
 
 scale=STRUCT([COLOR(BLACK)(scalaNord),scalaEst,COLOR(GREEN)(scalaSud),COLOR(YELLOW)(scalaOvest)])
 #VIEW(scale)
