be4ea4c0b127c9277bab470eeb3a9b915cad816c
j-hard <and.gia.83@gmail.com>
2014-05-17 17:40:27 +0200

    homework 3 commit 3

--- 2014-05-16/python/esercizio3.py
+++ 2014-05-16/python/esercizio3.py
@@ -13,8 +13,10 @@ from mapper import *
 from boolean import *
 from sysml import *
 
-def cycle(blocchi,merge,modifica,colorefont,sizefont,vedi):
-	bloccchiMod=[]
+
+#parametri (signature) array di blocchi, cella x il merge, array di blocchi x la modifica (mod[i] si sottrae a blocchi[i]), colore del fonte e size del font, flag x stampare a video 
+def mergingNumberingElimination(blocchi,merge,modifica):
+	blocchiMod=[]
 	
 	j=0
 	for blocco in blocchi:
@@ -22,39 +24,44 @@ def cycle(blocchi,merge,modifica,colorefont,sizefont,vedi):
 		mod=modifica[j]
 		V,CV = blocco
 		cell = MKPOL([bagno[0],[[v+1 for v in  bagno[1][toMerge]]],None])
-		#VIEW(STRUCT([hpc,cell]))
 		
-		#diagram = assemblyDiagramInit(x)([pts])
-		#diagram=assemblyDiagramInit([3,1,3])([[.4,.5,2],[.3],[1,1.2,.5]])
 		diagram=mod
-		bloccchiMod.append(diagram2cell(diagram,blocco,toMerge))	
+		blocchiMod.append(diagram2cell(diagram,blocco,toMerge))	
+		blocco=diagram2cell(diagram,blocco,toMerge)	
+		j=j+1
+		hpcb= SKEL_1(STRUCT(MKPOLS(blocco)))
+		hpcb = cellNumbering (blocco,hpcb)(range(len(blocco[1])),CYAN,1)		
+		VIEW(hpcb)
+	return blocchiMod		
+
+#parametri (signature) array di blocchi, cella x il merge, array di blocchi x la modifica (mod[i] si sottrae a blocchi[i]), colore del fonte e size del font, flag x stampare a video 
+def mergingNumberingEliminationAlt(blocchi,merge,modifica,colorefont,sizefont,vedi):
+	blocchiMod=[]
+	
+	j=0
+	for blocco in blocchi:
+		toMerge=merge[j] 
+		mod=modifica[j]
+		V,CV = blocco
+		cell = MKPOL([bagno[0],[[v+1 for v in  bagno[1][toMerge]]],None])
+		
+		diagram=mod
+		blocchiMod.append(diagram2cell(diagram,blocco,toMerge))	
 		blocco=diagram2cell(diagram,blocco,toMerge)	
 		j=j+1
 		if vedi==True:
 			hpcb= SKEL_1(STRUCT(MKPOLS(blocco)))
 			hpcb = cellNumbering (blocco,hpcb)(range(len(blocco[1])),colorefont,sizefont)		
 			VIEW(hpcb)
-	return bloccchiMod		
-
-
-
-
-
-
-
-
-
-
-
-
-
+	return blocchiMod		
 
 
 DRAW = COMP([VIEW,STRUCT,MKPOLS])
 
 bagno = assemblyDiagramInit([3,2,2])([[.3,3,.3],[3,.3],[.3,2.7]])
 modifica=assemblyDiagramInit([3,1,3])([[.4,.5,2],[.3],[1,1.2,.5]])
-b=cycle([bagno],[7],[modifica],CYAN,1,True)
+b=mergingNumberingEliminationAlt([bagno],[7],[modifica],CYAN,1,True)
+n=mergingNumberingElimination([bagno],[7],[modifica])
 d= SKEL_1(STRUCT(MKPOLS(b[0])))
 c = cellNumbering (b[0],d)(range(len(b[0][1])),CYAN,1)
 VIEW(c)
